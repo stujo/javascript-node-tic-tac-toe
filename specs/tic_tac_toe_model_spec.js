9e8d65d478e9061ec68f25776a8d48971730d987
Stujo <stujo@github.com>
2015-10-04 11:45:47 -0700

    Add single level of recursion to detect potential traps

--- specs/tic_tac_toe_model_spec.js
+++ specs/tic_tac_toe_model_spec.js
@@ -2,45 +2,61 @@ var Model = require('../lib/tic_tac_toe_model');
 
 describe('TicTacToeModel', function() {
 
+    describe('imagineStateIfIPlay', function(){
+       it('should mark X in 2th spot', function(){
+          var model = new Model("O--O-----", "X")
+          expect(model.imagineStateIfIPlay(2)).equal('O-XO-----');
+       });
+    });
+
     describe('presented with a winning opportunity', function() {
         it('should recommend completing the line at location 6', function() {
-            var model = Model("O--O-----", "O")
-            var result = model.boardSummary().recommendation.index;
-            expect(result).equal(6);
+            var model = new Model("O--O-----", "O")
+            var result = model.getRecommendation();
+            expect(result.recommendation).equal(6);
         });
         it('should recommend completing the line at location 0', function() {
-            var model = Model("--O-X--OX", "X")
-            var result = model.boardSummary().recommendation.index;
-            expect(result).equal(0);
+            var model = new Model("--O-X--OX", "X")
+            var result = model.getRecommendation();
+            expect(result.recommendation).equal(0);
         });
         it('should recommend completing the line at location 1', function() {
-            var model = Model("X-X-O--OX", "O")
-            var result = model.boardSummary().recommendation.index;
-            expect(result).equal(1);
+            var model = new Model("X-X-O--OX", "O")
+            var result = model.getRecommendation();
+            expect(result.recommendation).equal(1);
         });
     });
 
     describe('at the start of the game', function() {
         it('should recommend a center or a corner', function() {
-            var model = Model("---------", "O")
-            var result = model.boardSummary().recommendation.index;
-            expect(result).to.not.equal(2);
-            expect(result).to.not.equal(3);
-            expect(result).to.not.equal(5);
-            expect(result).to.not.equal(7);
+            var model = new Model("---------", "O")
+            var result = model.getRecommendation();
+            expect(result.recommendation).to.not.equal(1);
+            expect(result.recommendation).to.not.equal(3);
+            expect(result.recommendation).to.not.equal(5);
+            expect(result.recommendation).to.not.equal(7);
         });
     });
 
    describe('presented with a blocking opportunity', function() {
         it('should recommend blocking the line at location 6', function() {
-            var model = Model("O--O-----", "X")
-            var result = model.boardSummary().recommendation.index;
-            expect(result).equal(6);
+            var model = new Model("O--O-----", "X")
+            var result = model.getRecommendation();
+            expect(result.recommendation).equal(6);
         });
         it('should recommend blocking the line at location 0', function() {
-            var model = Model("--O-X--OX", "O")
-            var result = model.boardSummary().recommendation.index;
-            expect(result).equal(0);
+            var model = new Model("--O-X--OX", "O")
+            var result = model.getRecommendation();
+            expect(result.recommendation).equal(0);
+        });
+    });
+
+   describe('presented with a possible trap', function() {
+        it('should recommend forcing a placement over potential lines', function() {
+            var model = new Model("--O-X-O--", "X")
+            var result = model.getRecommendation();
+            expect(result.recommendation).to.not.equal(0);
+            expect(result.recommendation).to.not.equal(8);
         });
     });
 
